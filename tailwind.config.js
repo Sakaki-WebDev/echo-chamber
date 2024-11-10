56ba5b0387e7d601589441ef8faef3a49ace6053
Sakaki Lab <campbellcasteel@gmail.com>
2024-11-10 01:05:38 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "primary-500": "#0b5351",
+        "primary-600": "#9381ff",
+        "secondary-500": "#eca0ff",
+        "off-white": "#f6f7eb",
+        red: "#FF5A5A",
+        "dark-1": "#111d40",
+        "dark-2": "#0b132b",
+        "dark-3": "#264653",
+        "dark-4": "#264653",
+        "light-1": "#FFFFFF",
+        "light-2": "#fff",
+        "light-3": "#fff",
+        "light-4": "#ccc",
+      },
+      screens: {
+        xs: "480px",
+      },
+      width: {
+        420: "420px",
+        465: "465px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
