84901da1908fdd61c8b459dcf602d24bdbc04ccc
Sakaki Lab. <57556287+Sakaki-WebDev@users.noreply.github.com>
2024-11-11 22:41:11 +0000

    Update SigninForm.tsx

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -61,7 +61,12 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo2.svg" alt="logo" />
+         <img
+          src="/assets/images/logo2.png"
+          height={100}
+          width={175}
+          alt="logo"
+        />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
